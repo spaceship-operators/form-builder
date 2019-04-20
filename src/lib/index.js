0fc6244b11cb9192fd2140af6c2e2c03fa6ef531
Scott Hutchinson <scott@silverstripe.com>
2019-04-21 01:22:29 +1200

    Build out dist file for FormBuilder

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import FormBuilder from  '../containers/FormBuilder';
+
+export {
+  FormBuilder
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const Path = require('path');
+const PATHS = {
+  SRC: Path.resolve('src'),
+  DIST: Path.resolve('dist'),
+}
+
+module.exports = {
+  entry: {
+    bundle: `${PATHS.SRC}/containers/FormBuilder.js`,
+  },
+  output: {
+    path: PATHS.DIST,
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  externals: {
+    'react': 'commonjs react'
+  }
+};
